7c7090d49b667bf6cc4e40ceaea5b6cbd4e69c76
José Nildo de Macedo Pereira <Jos Nildo de Macedo Pereira>
2023-11-21 01:35:03 -0300

    Add: Game repository, service, controller, DTO

---
+++ src/main/java/br/com/josenildo/dslist/controllers/GameController.java
@@ -0,0 +1,24 @@
+package br.com.josenildo.dslist.controllers;
+
+import br.com.josenildo.dslist.dto.GameMinDTO;
+import br.com.josenildo.dslist.entities.Game;
+import br.com.josenildo.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameMinDTO> findAll() {
+        List<GameMinDTO> result = gameService.findAll();
+        return result;
+    }
+}
---
+++ src/main/java/br/com/josenildo/dslist/services/GameService.java
@@ -0,0 +1,27 @@
+package br.com.josenildo.dslist.services;
+
+import br.com.josenildo.dslist.dto.GameMinDTO;
+import br.com.josenildo.dslist.entities.Game;
+import br.com.josenildo.dslist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll() {
+        List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+
+    }
+}
+
+
+
+
+
